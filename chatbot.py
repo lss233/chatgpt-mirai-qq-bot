0f16408ad4fe89e5e9e4080321a663c9300ddc9f
1847905557 <1847905557@qq.com>
2023-01-01 13:58:56 +0800

    update import

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,8 @@
 from revChatGPT.ChatGPT import Chatbot
-from charset_normalizer import from_bytes
-from typing import Awaitable, Any, Dict, Tuple
+from graia.ariadne.app import Ariadne
+from graia.ariadne.model import Friend, Group
+from graia.ariadne.message import Source
+from typing import Union, Any, Dict, Tuple
 from config import Config
 from loguru import logger
 import os
