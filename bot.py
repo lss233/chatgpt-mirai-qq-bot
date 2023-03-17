c46709a3e63d366661bb4f644a4ae4e28f7ba8ee
lss233 <i@lss233.com>
2023-03-17 19:32:18 +0800

    refactor: use if..elif

--- bot.py
+++ bot.py
@@ -6,13 +6,13 @@ from loguru import logger
 sys.path.append(os.getcwd())
 
 from constants import config
+
 if config.onebot:
     logger.info("检测到 Onebot 配置，将以 Onebot 模式启动……")
     import onebot_bot
-if config.telegram:
+elif config.telegram:
     logger.info("检测到 telegram 配置，将以 telegram bot 模式启动……")
     import telegram_bot
 else:
     logger.info("检测到 mirai 配置，将以 mirai 模式启动……")
     import ariadne_bot
-
