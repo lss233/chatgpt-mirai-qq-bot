c31f9b5848b0ddf7e36e5c0b09cd971ffa631698
lss233 <8984680+lss233@users.noreply.github.com>
2023-04-02 22:05:58 +0800

    fix: won't continue if tts failed

--- utils/text_to_speech.py
+++ utils/text_to_speech.py
@@ -22,9 +22,9 @@ async def get_tts_voice(elem, conversation_context) -> Optional[Voice]:
         from utils.vits_tts import VitsAPI
         if config.mirai or config.onebot:
             output_file.name = output_file.name + ".silk"
-        await VitsAPI.vits_api(str(elem), output_file.name)
-        logger.debug(f"[TextToSpeech] 语音转换完成 - {output_file.name} - {conversation_context.session_id}")
-        return Voice(path=output_file.name)
+        if await VitsAPI.vits_api(str(elem), output_file.name):
+            logger.debug(f"[TextToSpeech] 语音转换完成 - {output_file.name} - {conversation_context.session_id}")
+            return Voice(path=output_file.name)
     elif "azure" == config.text_to_speech.engine:
         if await synthesize_speech(
                 str(elem),
