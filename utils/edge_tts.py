f2873bc44077f216015d08a304593e2d9a474cad
lss233 <8984680+lss233@users.noreply.github.com>
2023-04-06 11:59:18 +0800

    fix: stick to mp3 since we need ffmpeg anyway

--- utils/edge_tts.py
+++ utils/edge_tts.py
@@ -1,24 +1,12 @@
 from loguru import logger
+import edge_tts
 
 
-try:
-    import edge_tts
-    from pydub import AudioSegment
-
-
-    async def edge_tts_speech(text: str, voice_name: str, path: str):
-        try:
-            communicate = edge_tts.Communicate(text, voice_name)
-            await communicate.save(path + ".mp3")
-            sound = AudioSegment.from_mp3(path + ".mp3")
-            sound.export(path, format="wav")
-            return True
-        except Exception as err:
-            logger.error(f"edge-tts api error: ", err)
-            return False
-
-
-except FileNotFoundError as e:
-    async def edge_tts_speech(a=None, b=None, c=None):
-        logger.error("错误：edge-tts 服务无法加载，请安装最新的 edge-tts、pydub 和 ffmpeg 运行库。")
-        return None
+async def edge_tts_speech(text: str, voice_name: str, path: str):
+    try:
+        communicate = edge_tts.Communicate(text, voice_name)
+        await communicate.save(path + ".mp3")
+        return True
+    except Exception as err:
+        logger.error(f"[Edge TTS] API error: ", err)
+        return False
--- utils/text_to_speech.py
+++ utils/text_to_speech.py
@@ -39,8 +39,8 @@ async def get_tts_voice(elem, conversation_context) -> Optional[Voice]:
             logger.debug(f"[TextToSpeech] 语音转换完成 - {output_file.name} - {conversation_context.session_id}")
             return voice
     elif "edge" == config.text_to_speech.engine:
-        output_file.name = output_file.name
         if await edge_tts_speech(str(elem), conversation_context.conversation_voice, output_file.name):
+            output_file.name = output_file.name + ".mp3"
             voice = Voice(path=output_file.name)
             if config.mirai or config.onebot:
                 voice = Voice(data_bytes=await encode_to_silk(await voice.get_bytes()))
