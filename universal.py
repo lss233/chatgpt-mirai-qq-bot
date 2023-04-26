11e7a10ca1163e55ac5fa31b50a5ad4c9c38306e
lss233 <8984680+lss233@users.noreply.github.com>
2023-04-27 01:35:21 +0800

    fix: raise error

--- universal.py
+++ universal.py
@@ -42,79 +42,79 @@ async def get_ping_response(conversation_context: ConversationContext):
 async def handle_message(_respond: Callable, session_id: str, message: str,
                          chain: MessageChain = MessageChain("Unsupported"), is_manager: bool = False,
                          nickname: str = '某人'):
-    """正常聊天"""
-    if not message.strip():
-        return config.response.placeholder
+    try:
+        """正常聊天"""
+        if not message.strip():
+            return config.response.placeholder
 
-    for r in config.trigger.ignore_regex:
-        if re.match(r, message):
-            logger.debug(f"此消息满足正则表达式： {r}，忽略……")
-            return
+        for r in config.trigger.ignore_regex:
+            if re.match(r, message):
+                logger.debug(f"此消息满足正则表达式： {r}，忽略……")
+                return
 
-    # 此处为会话不存在时可以执行的指令
-    conversation_handler = await ConversationHandler.get_handler(session_id)
-    conversation_context = None
-    # 指定前缀对话
-    if ' ' in message and (config.trigger.allow_switching_ai or is_manager):
-        for ai_type, prefixes in config.trigger.prefix_ai.items():
-            for prefix in prefixes:
-                if f'{prefix} ' in message:
-                    conversation_context = await conversation_handler.first_or_create(ai_type)
-                    message = message.removeprefix(f'{prefix} ')
-                    break
-            else:
-                # Continue if the inner loop wasn't broken.
-                continue
-            # Inner loop was broken, break the outer.
-            break
-    if not conversation_handler.current_conversation:
-        conversation_handler.current_conversation = await conversation_handler.create(
-            config.response.default_ai)
+        # 此处为会话不存在时可以执行的指令
+        conversation_handler = await ConversationHandler.get_handler(session_id)
+        conversation_context = None
+        # 指定前缀对话
+        if ' ' in message and (config.trigger.allow_switching_ai or is_manager):
+            for ai_type, prefixes in config.trigger.prefix_ai.items():
+                for prefix in prefixes:
+                    if f'{prefix} ' in message:
+                        conversation_context = await conversation_handler.first_or_create(ai_type)
+                        message = message.removeprefix(f'{prefix} ')
+                        break
+                else:
+                    # Continue if the inner loop wasn't broken.
+                    continue
+                # Inner loop was broken, break the outer.
+                break
+        if not conversation_handler.current_conversation:
+            conversation_handler.current_conversation = await conversation_handler.create(
+                config.response.default_ai)
 
-    def wrap_request(n, m):
-        async def call(session_id, message, conversation_context, respond):
-            await m.handle_request(session_id, message, respond, conversation_context, n)
+        def wrap_request(n, m):
+            async def call(session_id, message, conversation_context, respond):
+                await m.handle_request(session_id, message, respond, conversation_context, n)
 
-        return call
+            return call
 
-    def wrap_respond(n, m):
-        async def call(session_id, message, rendered, respond):
-            await m.handle_respond(session_id, message, rendered, respond, n)
+        def wrap_respond(n, m):
+            async def call(session_id, message, rendered, respond):
+                await m.handle_respond(session_id, message, rendered, respond, n)
 
-        return call
+            return call
 
-    async def respond(msg: str):
-        if not msg:
-            return
-        ret = await _respond(msg)
-        for m in middlewares:
-            await m.on_respond(session_id, message, msg)
+        async def respond(msg: str):
+            if not msg:
+                return
+            ret = await _respond(msg)
+            for m in middlewares:
+                await m.on_respond(session_id, message, msg)
 
-        # TODO: 之后重构成 platforms 的 respond 只处理 MessageChain
-        if isinstance(msg, str):
-            msg = MessageChain([Plain(msg)])
+            # TODO: 之后重构成 platforms 的 respond 只处理 MessageChain
+            if isinstance(msg, str):
+                msg = MessageChain([Plain(msg)])
 
-        nonlocal conversation_context
-        if not conversation_context:
-            conversation_context = conversation_handler.current_conversation
+            nonlocal conversation_context
+            if not conversation_context:
+                conversation_context = conversation_handler.current_conversation
 
-        # TTS Converting
-        if conversation_context.conversation_voice and isinstance(msg, MessageChain):
-            tasks = []
-            for elem in msg:
-                task = asyncio.create_task(get_tts_voice(elem, conversation_context))
-                tasks.append(task)
-            while tasks:
-                done, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-                for voice_task in done:
-                    voice = await voice_task
-                    if voice:
-                        await _respond(voice)
+            # TTS Converting
+            if conversation_context.conversation_voice and isinstance(msg, MessageChain):
+                tasks = []
+                for elem in msg:
+                    task = asyncio.create_task(get_tts_voice(elem, conversation_context))
+                    tasks.append(task)
+                while tasks:
+                    done, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+                    for voice_task in done:
+                        voice = await voice_task
+                        if voice:
+                            await _respond(voice)
 
-        return ret
+            return ret
 
-    async def request(_session_id, prompt: str, conversation_context, _respond):
-        try:
+        async def request(_session_id, prompt: str, conversation_context, _respond):
             task = None
 
             # 不带前缀 - 正常初始化会话
@@ -238,36 +238,38 @@ async def handle_message(_respond: Callable, session_id: str, message: str,
                     await action(session_id, prompt, rendered, respond)
             for m in middlewares:
                 await m.handle_respond_completed(session_id, prompt, respond)
-        except DrawingFailedException as e:
-            logger.exception(e)
-            await respond(config.response.error_drawing.format(exc=e.__cause__  or '未知'))
-        except CommandRefusedException as e:
-            await respond(str(e))
-        except openai.error.InvalidRequestError as e:
-            await respond(f"服务器拒绝了您的请求，原因是： {str(e)}")
-        except BotOperationNotSupportedException:
-            await respond("暂不支持此操作，抱歉！")
-        except ConcurrentMessageException as e:  # Chatbot 账号同时收到多条消息
-            await respond(config.response.error_request_concurrent_error)
-        except BotRatelimitException as e:  # Chatbot 账号限流
-            await respond(config.response.error_request_too_many.format(exc=e))
-        except NoAvailableBotException as e:  # 预设不存在
-            await respond(f"当前没有可用的{e}账号，不支持使用此 AI！")
-        except BotTypeNotFoundException as e:  # 预设不存在
-            respond_msg = f"AI类型{e}不存在，请检查你的输入是否有问题！目前仅支持：\n"
-            respond_msg += botManager.bots_info()
-            await respond(respond_msg)
-        except PresetNotFoundException:  # 预设不存在
-            await respond("预设不存在，请检查你的输入是否有问题！")
-        except (RequestException, SSLError, ProxyError, MaxRetryError, ConnectTimeout, ConnectTimeout, httpcore.ReadTimeout) as e:  # 网络异常
-            await respond(config.response.error_network_failure.format(exc=e))
-        except Exception as e:  # 未处理的异常
-            logger.exception(e)
-            await respond(config.response.error_format.format(exc=e))
 
-    action = request
-    for m in middlewares:
-        action = wrap_request(action, m)
 
-    # 开始处理
-    await action(session_id, message.strip(), conversation_context, respond)
+        action = request
+        for m in middlewares:
+            action = wrap_request(action, m)
+
+        # 开始处理
+        await action(session_id, message.strip(), conversation_context, respond)
+    except DrawingFailedException as e:
+        logger.exception(e)
+        await respond(config.response.error_drawing.format(exc=e.__cause__ or '未知'))
+    except CommandRefusedException as e:
+        await respond(str(e))
+    except openai.error.InvalidRequestError as e:
+        await respond(f"服务器拒绝了您的请求，原因是： {str(e)}")
+    except BotOperationNotSupportedException:
+        await respond("暂不支持此操作，抱歉！")
+    except ConcurrentMessageException as e:  # Chatbot 账号同时收到多条消息
+        await respond(config.response.error_request_concurrent_error)
+    except BotRatelimitException as e:  # Chatbot 账号限流
+        await respond(config.response.error_request_too_many.format(exc=e))
+    except NoAvailableBotException as e:  # 预设不存在
+        await respond(f"当前没有可用的{e}账号，不支持使用此 AI！")
+    except BotTypeNotFoundException as e:  # 预设不存在
+        respond_msg = f"AI类型{e}不存在，请检查你的输入是否有问题！目前仅支持：\n"
+        respond_msg += botManager.bots_info()
+        await respond(respond_msg)
+    except PresetNotFoundException:  # 预设不存在
+        await respond("预设不存在，请检查你的输入是否有问题！")
+    except (RequestException, SSLError, ProxyError, MaxRetryError, ConnectTimeout, ConnectTimeout,
+            httpcore.ReadTimeout) as e:  # 网络异常
+        await respond(config.response.error_network_failure.format(exc=e))
+    except Exception as e:  # 未处理的异常
+        logger.exception(e)
+        await respond(config.response.error_format.format(exc=e))
