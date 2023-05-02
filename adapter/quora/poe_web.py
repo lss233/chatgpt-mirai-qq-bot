0a23e6347b003fb4a68d461afe1075e4222dda92
lss233 <8984680+lss233@users.noreply.github.com>
2023-05-02 10:54:35 +0800

    feat: poe will support multi-conversation on specified bot type

--- adapter/quora/poe_web.py
+++ adapter/quora/poe_web.py
@@ -52,15 +52,6 @@ class BotType(Enum):
             None,
         )
 
-
-class PoeClientWrapper:
-    def __init__(self, client_id: int, client: PoeClient, p_b: str):
-        self.client_id = client_id
-        self.client = client
-        self.p_b = p_b
-        self.last_ask_time = None
-
-
 class PoeAdapter(BotAdapter):
 
     account: PoeCookieAuth
@@ -69,22 +60,54 @@ class PoeAdapter(BotAdapter):
         """获取内部队列"""
         super().__init__(session_id)
         self.session_id = session_id
-        self.bot_type = bot_type or BotType.ChatGPT
+        self.bot_name = bot_type.value
         self.account = account_manager.pick("poe-token")
-        self.account.get_client().create_bot()
+        self.client = self.account.get_client()
+        self.custom_bot = False
+        if self.bot_name in [BotType.ChatGPT, BotType.Claude]:
+            # Create new bot or use default one?
+            self.custom_bot = {
+                "handle": self.bot_name,
+                "base_model": bot_type.value,
+                "prompt": r"You're a helpful assistant.",
+                "prompt_public": False,
+                "description": f"ChatGPT for Bot: session_id={session_id}, base_model={bot_type.value}",
+                "suggested_replies": False,
+                "private": False
+            }
+            self.client.create_bot(**self.custom_bot)
 
     async def ask(self, msg: str) -> Generator[str, None, None]:
         """向 AI 发送消息"""
         # 覆盖 PoeClient 内部的锁逻辑
-        while None in self.account.client.active_messages.values():
+        while None in self.client.active_messages.values():
             await asyncio.sleep(0.01)
-        for final_resp in self.account.client.send_message(chatbot=self.bot_type.value, message=msg):
+        for final_resp in self.client.send_message(chatbot=self.bot_name.value, message=msg):
             yield final_resp["text"]
 
     async def rollback(self):
         """回滚对话"""
-        self.account.client.purge_conversation(self.bot_type.value, 2)
+        self.client.purge_conversation(self.bot_name.value, 2)
 
     async def on_destoryed(self):
         """当会话被重置时，此函数被调用"""
-        self.account.client.send_chat_break(self.bot_type.value)
+        self.client.send_chat_break(self.bot_name.value)
+        if self.custom_bot:
+            # TODO: delete the bot
+            pass
+
+    async def preset_ask(self, role: str, text: str):
+        if role.endswith('bot') or role in {'assistant', 'poe'}:
+            logger.debug(f"[预设] 响应：{text}")
+            yield text
+        else:
+            if role == 'system' and self.custom_bot:
+                self.custom_bot['prompt'] = text
+                self.client.edit_bot(**self.custom_bot)
+                return
+            logger.debug(f"[预设] 发送：{text}")
+            item = None
+            async for item in self.ask(text): ...
+            if item:
+                logger.debug(f"[预设] Chatbot 回应：{item}")
+
