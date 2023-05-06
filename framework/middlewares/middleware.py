8032188342d42b1f756fc283bd2284b33690fee2
lss233 <8984680+lss233@users.noreply.github.com>
2023-05-06 11:06:28 +0800

    refactor: reduce paramters count in middlewares

--- framework/middlewares/middleware.py
+++ framework/middlewares/middleware.py
@@ -5,10 +5,10 @@ from framework.request import Response, Request
 
 class Middleware:
     async def handle_request(self, request: Request, response: Response, _next: Callable):
-        pass
+        return await _next(request, response)
 
     async def handle_respond(self, request: Request, response: Response, _next: Callable):
-        pass
+        return await _next(request, response)
 
     async def on_respond(self, *args, **kwargs):
         pass
