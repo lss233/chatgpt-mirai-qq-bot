678f8fe93627870fa4e932e6514020f27634a3ba
lss233 <8984680+lss233@users.noreply.github.com>
2023-05-10 01:02:56 +0800

    refactor(http-api): use single quart instance

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-
 sys.path.append(os.getcwd())
 from framework.accounts import account_manager
 
@@ -42,11 +41,6 @@ if constants.config.mirai:
 
     bots.append(start_task())
 
-if constants.config.onebot:
-    logger.info("检测到 Onebot 配置，将启动 Onebot 模式……")
-    from framework.platforms.onebot_bot import start_task
-
-    bots.append(start_task())
 if constants.config.telegram:
     logger.info("检测到 telegram 配置，将启动 telegram bot 模式……")
     from framework.platforms.telegram_bot import start_task
@@ -57,16 +51,24 @@ if constants.config.discord:
     from framework.platforms.discord_bot import start_task
 
     bots.append(start_task())
-if constants.config.http:
-    logger.info("检测到 http 配置，将启动 http service 模式……")
-    from framework.platforms.http_service import start_task
 
-    bots.append(start_task())
-if constants.config.wecom:
-    logger.info("检测到 Wecom 配置，将启动 Wecom Bot 模式……")
-    from framework.platforms.wecom_bot import start_task
 
-    bots.append(start_task())
+async def setup_web_service():
+    from framework.platforms.onebot_bot import start_http_app
+    from framework.platforms.http_service import route as routes_http
+    from framework.platforms.http_service_legacy import route as routes_http_legacy
+    logger.info("启动 HTTP API……")
+    app = await start_http_app()
+    routes_http(app)
+    routes_http_legacy(app)
+
+    if constants.config.wecom:
+        logger.info("检测到 Wecom 配置，将注册 Wecom 路由……")
+        from framework.platforms.wecom_bot import route as routes_wecom
+        routes_wecom(app)
+
+
+bots.append(setup_web_service())
 
 loop.run_until_complete(asyncio.gather(*bots))
 loop.run_forever()
