b69a4c02f53de73e8746e67dec6083dca3d97f09
Edward <73746306+WangEdward@users.noreply.github.com>
2023-05-15 11:48:30 +0800

    fix(llm): bing livecheck

--- framework/llm/microsoft/models.py
+++ framework/llm/microsoft/models.py
@@ -1,6 +1,8 @@
 import httpx
+import json
 from pydantic import Field
 
+from EdgeGPT import Chatbot as EdgeChatbot
 import constants
 from framework.accounts import AccountInfoBaseModel
 
@@ -20,13 +22,17 @@ class BingCookieAuth(AccountInfoBaseModel):
         }
 
     async def check_alive(self) -> bool:
-        async with httpx.AsyncClient(
-                trust_env=True,
-                headers={
-                    "Cookie": self.cookie_content,
-                    "sec-ch-ua": r'"Chromium";v="112", "Microsoft Edge";v="112", "Not:A-Brand";v="99"',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64'
-                }
-        ) as client:
-            response = await client.get(f"{constants.config.bing.bing_endpoint}")
-            return "Success" in response.text
+        bot = None
+        bot = await EdgeChatbot.create(cookies=json.loads(self.cookie_content))
+        return bot is not None
+        # cookie_content 的格式不可以直接作为 header，所以不能直接用 httpx 来测试
+        # async with httpx.AsyncClient(
+        #         trust_env=True,
+        #         headers={
+        #             # "Cookie": self.cookie_content,
+        #             "sec-ch-ua": r'"Chromium";v="112", "Microsoft Edge";v="112", "Not:A-Brand";v="99"',
+        #             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64'
+        #         }
+        # ) as client:
+        #     response = await client.get(f"{constants.config.bing.bing_endpoint}")
+        #     return "Success" in response.text
