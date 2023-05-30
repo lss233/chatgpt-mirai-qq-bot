e34fd946ce8a7dcbec2ba4290aeb70e900c0033a
Cloxl <88774611+Cloxl@users.noreply.github.com>
2023-05-30 18:32:37 +0800

    poe:beaver→gpt4

--- constants.py
+++ constants.py
@@ -12,7 +12,7 @@ botManager = BotManager(config)
 class LlmName(Enum):
     SlackClaude = "slack-claude"
     PoeSage = "poe-sage"
-    PoeGPT4 = "poe-beaver"
+    PoeGPT4 = "poe-gpt4"
     PoeClaude2 = "poe-claude2"
     PoeClaude = "poe-claude"
     PoeClaude100k = "poe-claude100k"
