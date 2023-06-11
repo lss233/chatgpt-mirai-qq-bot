da8a470f2ac757d56db5214d0ab17587d4e59708
lss233 <8984680+lss233@users.noreply.github.com>
2023-06-11 14:24:22 +0800

    fix: remove unused logs

--- adapter/chatgpt/web.py
+++ adapter/chatgpt/web.py
@@ -96,7 +96,7 @@ class ChatGPTWebAdapter(BotAdapter):
                 # 确保是当前的会话，才更新 parent_id
                 if self.conversation_id == resp["conversation_id"]:
                     self.parent_id = resp["parent_id"]
-                logger.debug("resp = " + str(resp))
+
                 yield resp["message"]
             if last_response:
                 logger.debug(f"[ChatGPT-Web] {last_response['conversation_id']} - {last_response['message']}")
